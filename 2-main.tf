fc452cf9b00f78b4a59db992e7b5a188d1b9b45b
cquaye <kaptinq@gmail.com>
2026-03-24 22:02:02 -0700

    class assignment

--- 2-main.tf
+++ 2-main.tf
@@ -14,3 +14,28 @@ resource "aws_s3_object" "file1" {
   key = "approval.PNG"
   source = "Theo_approval.PNG"
 }
+
+resource "aws_s3_object" "file1" {
+  bucket = aws_s3_bucket.frontend.id
+  key = "jenkins1.PNG"
+  source = "jenkins_deploy1.PNG"
+}
+
+resource "aws_s3_object" "file1" {
+  bucket = aws_s3_bucket.frontend.id
+  key = "jenkins2.PNG"
+  source = "jenkins_deploy2.PNG"
+}
+
+resource "aws_s3_object" "file1" {
+  bucket = aws_s3_bucket.frontend.id
+  key = "jenkins3.PNG"
+  source = "jenkins_deploy3.PNG"
+}
+
+
+resource "aws_s3_object" "file1" {
+  bucket = aws_s3_bucket.frontend.id
+  key = "webhook.PNG"
+  source = "webhook.PNG"
+}
