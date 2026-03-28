f9326f4d324c6e05930a77bcbbdbefed63a8be6a
cquaye <kaptinq@gmail.com>
2026-03-28 12:15:27 -0700

    class assignment

--- 2-main.tf
+++ 2-main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_public_access_block" "s3_permissions" {
 
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = false
 
 }
