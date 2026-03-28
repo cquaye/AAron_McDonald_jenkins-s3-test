7e29b2038e7cac487dc83c05e1087dfffc0bfcf2
cquaye <kaptinq@gmail.com>
2026-03-28 12:12:31 -0700

    class assignment

--- 2-main.tf
+++ 2-main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "frontend" {
   bucket_prefix = "my-jenkins-bucket-"
   force_destroy = true
-  
+
 
   tags = {
     Name = "Jenkins Bucket"
@@ -9,39 +9,60 @@ resource "aws_s3_bucket" "frontend" {
 }
 
 
+#---permissions for public access---#    
+resource "aws_s3_bucket_public_access_block" "s3_permissions" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+
+#---versioning ---#  
+resource "aws_s3_bucket_versioning" "se_versioning" {
+  bucket = aws_s3_bucket.frontend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  
+}
+
 resource "aws_s3_object" "file1" {
   bucket = aws_s3_bucket.frontend.id
-  key = "approval.PNG"
+  key    = "approval.PNG"
   source = "Theo_approval.PNG"
 }
 
 resource "aws_s3_object" "file2" {
   bucket = aws_s3_bucket.frontend.id
-  key = "jenkins1.PNG"
+  key    = "jenkins1.PNG"
   source = "jenkins_deploy_1.PNG"
 }
 
 resource "aws_s3_object" "file3" {
   bucket = aws_s3_bucket.frontend.id
-  key = "jenkins2.PNG"
+  key    = "jenkins2.PNG"
   source = "jenkins_deploy_2.PNG"
 }
 
 resource "aws_s3_object" "file4" {
   bucket = aws_s3_bucket.frontend.id
-  key = "jenkins3.PNG"
+  key    = "jenkins3.PNG"
   source = "jenkins_deploy_3.PNG"
 }
 
 
 resource "aws_s3_object" "file5" {
   bucket = aws_s3_bucket.frontend.id
-  key = "webhook.PNG"
+  key    = "webhook.PNG"
   source = "webhook.PNG"
 }
 
 resource "aws_s3_object" "file6" {
   bucket = aws_s3_bucket.frontend.id
-  key = "armageddon_proof.PNG"
+  key    = "armageddon_proof.PNG"
   source = "armageddon_proof.PNG"
 }
