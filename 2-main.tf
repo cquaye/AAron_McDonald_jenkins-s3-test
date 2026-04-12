ad20ec4496da3b452b7c4c8706f07586efb9820e
cquaye <kaptinq@gmail.com>
2026-04-11 17:09:33 -0700

    lab with charles inescure artifacts

--- 2-main.tf
+++ 2-main.tf
@@ -1,90 +1,46 @@
-resource "aws_s3_bucket" "frontend" {
-  bucket_prefix = "my-jenkins-bucket-"
-  force_destroy = true
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "totally-insecure-demo-bucket"
+}
 
+resource "aws_s3_bucket_public_access_block" "insecure_bucket" {
+  bucket = aws_s3_bucket.insecure_bucket.id
 
-  tags = {
-    Name = "Jenkins Bucket"
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_security_group" "wide_open" {
+  name = "wide-open-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_iam_policy" "wildcard_policy" {
+  name = "wildcard-policy"
 
-# #---permissions for public access---#    
-# resource "aws_s3_bucket_public_access_block" "s3_permissions" {
-#   bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "*"
+      Resource = "*"
+    }]
+  })
+}
 
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
+resource "aws_instance" "no_imdsv2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 
-# }
+  metadata_options {
+    http_tokens = "optional"
+  }
+}
 
-
-#---create s3 bucket policy---# 
-# resource "aws_s3_bucket_policy" "s3_policy" {
-#   bucket = aws_s3_bucket.frontend.id
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect = "Allow"
-#       Principal = "*"
-#       Action = "s3:GetObject"
-#       Resource = "${aws_s3_bucket.frontend.arn}/*"
-
-
-#     }
-#     ]
-#   })
-  
-# }
-
-# resource "aws_s3_object" "file1" {
-#   bucket = aws_s3_bucket.frontend.id
-#   key    = "approval.PNG"
-#   source = "Theo_approval.PNG"
-#   content_type = "image/png"
-
-  
-# }
-
-# resource "aws_s3_object" "file2" {
-#   bucket = aws_s3_bucket.frontend.id
-#   key    = "jenkins1.PNG"
-#   source = "jenkins_deploy_1.PNG"
-#   content_type = "image/png"
-  
-# }
-
-# resource "aws_s3_object" "file3" {
-#   bucket = aws_s3_bucket.frontend.id
-#   key    = "jenkins2.PNG"
-#   source = "jenkins_deploy_2.PNG"
-#   content_type = "image/png"
-  
-# }
-
-# resource "aws_s3_object" "file4" {
-#   bucket = aws_s3_bucket.frontend.id
-#   key    = "jenkins3.PNG"
-#   source = "jenkins_deploy_3.PNG"
-#   content_type = "image/png"
-  
-# }
-
-
-# resource "aws_s3_object" "file5" {
-#   bucket = aws_s3_bucket.frontend.id
-#   key    = "webhook.PNG"
-#   source = "webhook.PNG"
-#   content_type = "image/png"
-  
-# }
-
-# resource "aws_s3_object" "file6" {
-#   bucket = aws_s3_bucket.frontend.id
-#   key    = "armageddon_proof.PNG"
-#   source = "armageddon_proof.PNG"
-#   content_type = "image/png"
-  
-# }
